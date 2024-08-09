70c7dec379d587b4f9b254cf3d511f689805a155
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-08-09 19:47:22 +0530

    did something with text

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const serif = Roboto_Serif({weight:"400",subsets:["latin"]});
 export default function Home(){
   const [nav,setNav] = useState(null);
   const [para,setPara] = useState("");
-  
+
   const word = "It is astonishing to see how billions of atoms could bond together in perfect combinations to form a single DNA – the core of a human life. This has been the inspiration behind seven-Pitstops-For-Change (7p4c), the world's first such journey for transformation set in the backdrop of incredible India. Be ready to experience hundreds of tiny and big travel experiences in India, woven together in the perfect combination and proportion and to be experienced in the perfect environment with the perfect blend of people with unimaginably diverse backgrounds.";
   useEffect(()=>{
     let i = 0;
@@ -20,7 +20,7 @@ export default function Home(){
     },[10])
   },[])
   return(<>
-  <div className={`absolute h-[100vh] w-[100vw] backdrop-blur-sm ${nav?"block":"hidden"}`}></div>
+  <div className={`absolute h-[100vh] w-[100vw] backdrop-blur-sm ${nav?"block":"hidden"}`} onClick={()=>{setNav(!nav)}}></div>
   <div className={`list-none bg-[#F1EDEB] fixed w-full h-full flex ${nav!=null?nav?"navin":"navout":"hidden"} xl:pl-20 pl-6 flex-col justify-center gap-5 text-7xl`} style={serif.style}>
       <li className={`${nav!=null?nav?"first":"opacity-0 transition":"hidden"}`}>Index</li>
       <li className={`${nav!=null?nav?"second":"opacity-0 transition":"hidden"}`}>Journey</li>
@@ -38,10 +38,16 @@ export default function Home(){
     <div>
     </div>
   </div>
-  <div className={`pt-20 px-5 text-white ${font.className} xl:ml-10 xl:pt-40`}>
+  <div className={`pt-20 px-5 text-white ${font.className} xl:ml-10 xl:pt-40 h-[80vh]`}>
   <h1 className="mt-10 text-7xl">7 Pistons for change</h1>
   <h1 className="mt-5 xl:w-[50%]">{para}</h1>
   <img src="dna.png" alt="" className="xl:h-60 floating xl:right-20 rotate-45 xl:top-52 absolute -z-30 h-40 top-72 blur-sm xl:blur-0" />
+  </div>
+  <div className={`text-white ${serif.className} px-5 xl:ml-10`}>
+  <h1 className={`text-sm image-bg`}>See below how 7p4c with its chemistry of perfection can leave you with a newborn vision, impact and wisdom to lead life.</h1>
+  <div className="flex">
+
+  </div>
   </div>
   </>)
 }
