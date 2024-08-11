5a936fdf5583875b26e0ee8b7803891c7e61f1bf
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-08-11 14:37:00 +0530

    bg-change

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -5,9 +5,6 @@ const bebas = Bebas_Neue({ weight: "400", subsets: ["latin"] })
 export default function Page() {
     return (
         <>
-            <div className="absolute h-[100vh] w-[100vw] top-0 -z-20 overflow-hidden">
-                <video className="object-cover w-full h-full" src="https://ia601407.us.archive.org/31/items/y-2meta.app-india-raw-beauty-in-cinematic/Y2meta.app-India%20-%20Raw%20Beauty%20-%20in%20Cinematic.mp4" muted autoPlay loop></video>
-            </div>
             <div className={`text-white ${bebas.className}  mt-10 px-5`}>
                 <h1 className="text-center flag text-5xl">Register For Audio / Video Discussion</h1>
                 <div className="bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 mx-auto xl:mt-20 flex items-center justify-center xl:w-[500px] py-10 rounded-2xl flex-col gap-5">
